092af7640fb29783cae5423ba5a1716d6c8189cd
Dong jae <50938226+dj9308@users.noreply.github.com>
2020-05-07 12:23:30 +0900

    200507

---
+++ 백준/20_05_1째주/No_2193.java
@@ -0,0 +1,29 @@
+package may_first;
+
+import java.util.Scanner;
+
+// int => long
+
+public class No_2193 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		long memo[][] = new long[90+1][2];
+		memo[1][1] = memo[2][0] = 1;
+		
+		for(int i=3;i<=90;i++) {
+			for(int j=0;j<=1;j++) {
+				if(memo[i-1][1]>0 && j==1) memo[i][j] = memo[i-1][0];
+				else memo[i][j] = memo[i-1][0]+memo[i-1][1];
+			}
+		}
+		
+		long answer = 0;
+		for(int i=0;i<=1;i++) {
+			answer+=memo[N][i];
+		}
+		System.out.println(answer);
+		sc.close();
+	}
+}
