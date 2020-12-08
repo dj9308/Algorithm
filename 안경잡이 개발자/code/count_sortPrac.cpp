5160720ec1493eca32dea251ecf70237623c1c0f
Dong jae <50938226+dj9308@users.noreply.github.com>
2020-12-08 20:10:05 +0900

    20201208

--- 안경잡이 개발자/code/count_sortPrac.cpp
+++ 안경잡이 개발자/code/count_sortPrac.cpp
@@ -3,25 +3,29 @@
 int main(void){
 	int temp;
 	int count[5];
+	
 	int array[30] = {
 	1,3,2,4,3,2,6,3,1,2,
 	3,4,4,3,5,1,2,3,5,2,
 	3,1,4,3,5,1,2,1,1,1};
 	
 	for(int i=0;i<5;i++){
-		count[i] =0;
-	}
+			count[i]=0;
+	};
 	
+	// 배열에 담기 
 	for(int i=0;i<30;i++){
-		count[array[i]-1]++;
-	}
+			count[array[i]-1]++;
+	};
 	
 	for(int i=0;i<5;i++){
-		if(count[i] !=0){
-			for(int j = 0;j<count[i];j++){
+		if(count[i]!=0){
+			// count 배열의 갯수 만큼 j에 적용해 출력 
+			for(int j=0;j<count[i];j++){
 				printf("%d",i+1);
-			}
-		};
+			}	
+		}
 	}
+	
 	return 0;
 }
