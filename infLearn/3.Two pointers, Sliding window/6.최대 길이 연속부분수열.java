feb5b2021d08de7ec2e05e1f6f1221b6aff57157
Dong jae <50938226+dj9308@users.noreply.github.com>
2022-10-07 23:40:04 +0900

    20221007
    말랑말랑 tow pointers

---
+++ infLearn/3.Two pointers, Sliding window/6.최대 길이 연속부분수열.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > m) {
+                if(arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(main.solution(n, m, arr));
+
+    }
+}
