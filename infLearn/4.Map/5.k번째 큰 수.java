5e210f7a6efcd1e66ef0c751e7915d4b405cb038
Dong jae <50938226+dj9308@users.noreply.github.com>
2022-10-15 22:32:55 +0900

    20221015
    말랑말랑

---
+++ infLearn/4.Map/5.k번째 큰 수.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+	public int solution(String input1, String input2) {
+		int answer = 0;
+		char[] array = input1.toCharArray();
+
+		for (int i = 0; i < array.length; i++) {
+			
+		}
+
+		return answer;
+
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		Scanner in = new Scanner(System.in);
+		String input1 = in.nextLine();
+		String input2 = in.nextLine();
+
+		System.out.println(main.solution(input1, input2));
+
+	}
+}
